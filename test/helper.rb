682bf1d29726f22f316b821eac944df5bb07f2ca
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-02-02 19:25:21 +0900

    init

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,8 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'fluent-plugin-growthforecast'
+require 'fluent/test'
+require 'fluent/plugin/out_growthforecast'
 
 class Test::Unit::TestCase
 end
